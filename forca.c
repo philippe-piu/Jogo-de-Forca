e7f5ac788a197ad0afd2bca7cc3e22214360ccbf
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-04-07 19:45:23 -0300

    Jogo-de-Forca

--- forca.c
+++ forca.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
 #include<string.h>
+#include "forca.h"
 
+//Variaveis Globais
 char palavrasecreta[20];
 char chutestentativas[26];
-int tentativas = 0;
-
-//Funções
-void apresentacao();
-void chuta(char chutestentativas[26], int *tentativas);
-int jachutou(char letra);
-void desenhaforca();
-void escolhasecreta();
+int chutesdados = 0;
 
 //Jogo da Forca
 int main()
@@ -25,10 +20,10 @@ int main()
 	do
 	{
 		desenhaforca();
-		chuta(chutestentativas, &tentativas);
-		
+		chuta(chutestentativas, &chutesdados);
+
 	}
-	while(!acertou && !enforcou);
+	while(!acertouusuario() && !enforcouusuario());
 	//Quando tem isso !acertou ele assume como um booleano true e false
 	return(0);
 }
@@ -44,13 +39,13 @@ void apresentacao()
 
 
 //Chute do Usuario
-void chuta(char chutestentativas[26], int *tentativas)
+void chuta(char chutestentativas[26], int *chutesdados)
 {
 	char chute;
 	printf("\nInforme uma letra para forca: ");
 	scanf(" %c", &chute);
-	chutestentativas[(*tentativas)] =  chute;
-	(*tentativas)++;
+	chutestentativas[(*chutesdados)] =  chute;
+	(*chutesdados)++;
 }
 
 //Se você chutou
@@ -58,7 +53,7 @@ int jachutou(char letra)
 {
 	int j, achou = 0;
 
-	for(j = 0; j < tentativas; j++)
+	for(j = 0; j < chutesdados; j++)
 	{
 		if(chutestentativas[j] == letra)
 		{
@@ -66,17 +61,17 @@ int jachutou(char letra)
 			break;
 		}
 	}
-	
+
 	return(achou);
 }
 
-//Desenho da forca
+//Função do Desenho da forca
 void desenhaforca()
 {
 	int i;
 	printf("\n");
-	
-	printf("Voce deu %d chutes\n", tentativas);
+
+	printf("Voce deu %d chutes\n", chutesdados);
 
 	for(i = 0; i < strlen(palavrasecreta); i++)
 	{
@@ -90,13 +85,77 @@ void desenhaforca()
 		}
 	}
 	printf("\n");
-	
+
 }
 
-//Palavra secreta
+//Função da Palavra secreta
 void escolhasecreta()
 {
-	sprintf(palavrasecreta, "MELANCIA");
+	int i;
+	FILE *f;
+	//Pegando arquivo com as palavras secretas
+	f = fopen("palavrasecreta.txt","r");
+	
+	if(f == 0){
+		printf("Banco de Dados não disponivel no momento tente mais tarde\n");
+		exit(1);
+	}
+	
+	int qtddepalavras;
+	fscanf(f, "%d", &qtddepalavras);
+	
+	srand(time(0));
+	//não tem nada ver com numeros é so um nome de variavel
+	int numerorandomico = rand() % qtddepalavras;
+	
+	for(i =0; i < numerorandomico; i++){
+		fscanf(f, "%s", palavrasecreta);
+	}
+	
+	//Fechando arquivo com as palavras secretas
+	fclose(f);
 }
 
+//Função de finalização de jogo se usuario perdeu
+int enforcouusuario()
+{
+	int i, j, existe, erros;
+	erros = 0;
 
+	//Loop em todos os chutes do usuario
+	for(i = 0; i < chutesdados; i++)
+	{
+		existe = 0;
+		
+		//Aqui estou verificando quantas letras existem na palavra secreta e se ela é igual a o chute do usuario
+		for(j = 0; j < strlen(palavrasecreta); j++)
+		{
+			//Se a palavra foi encontrada quebro o loop
+			if(chutestentativas[i] == palavrasecreta[j])
+			{
+				existe = 1;
+				break;
+			}
+		}
+		//Aqui verifico as palavras que não existem na palavra secreta
+		if(!existe) erros++;
+	}
+		
+	//Se o erros for maior ou igual a 5 Jogador perdeu
+	return erros >= 5;
+}
+
+//Função finaliza o jogo caso usuario ganhou
+int acertouusuario(){
+	int i;
+	for(i = 0; i < strlen(palavrasecreta); i++){
+		//Se o usuario não chutou a palavra secreta retorna 0
+		if(!jachutou(palavrasecreta[i])){
+			return 0;
+		}
+	}
+	
+	//Se usuario já acertou todas as palavras retorno 1 para acabar o jogo
+	
+	return 1;
+}
---
+++ forca.h
@@ -0,0 +1,8 @@
+//Funções
+void apresentacao();
+void chuta(char chutestentativas[26], int *chutesdados);
+int jachutou(char letra);
+void desenhaforca();
+void escolhasecreta();
+int enforcouusuario();
+int acertouusuario();
